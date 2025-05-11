a6dbf429ba86a6d0297a854de3e85148b599f7d7
Amelito Alcuitas Jr <amelitoalcuitasjr@gmail.com>
2025-05-11 15:02:58 +0800

    docker improvement

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,6 +6,17 @@ export default defineConfig({
   server: {
     port: 3000,
     host: true,
+    hmr: {
+      clientPort: 3000,
+      host: "0.0.0.0",
+    },
+    watch: {
+      usePolling: true,
+      interval: 1000,
+    },
   },
   plugins: [react()],
+  optimizeDeps: {
+    force: false,
+  },
 });
